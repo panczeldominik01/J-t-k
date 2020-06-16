bf3216dd3cc2ba788bb0e8393d17735299f7ec82
panczeldominik01 <panczdomii@gmail.com>
2020-06-16 18:07:09 +0200

    Second Commit

--- Szabaduloszoba/Szabaduloszoba/Program.cs
+++ Szabaduloszoba/Szabaduloszoba/Program.cs
@@ -16,11 +16,12 @@ namespace Szabaduloszoba
             string leltár1 = "";
             string leltár2 = "";          
             int ablak = 0;
+            int bot = 0;
             string irany = " ";
             while (ablak == 0 || irany != "észak")
             {
                 Console.WriteLine("Leltár : {0}  {1}",leltár1, leltár2);
-                Console.WriteLine("Mit szerentél tenni? ");
+                Console.WriteLine("Mit szerentél tenni? (nézd, menj, tör, mentes vedd fel)");
                 string lepes = Convert.ToString(Console.ReadLine());
                 if (lepes == "tör")
                 {
@@ -36,12 +37,29 @@ namespace Szabaduloszoba
                 }
                 if (lepes == "menj")
                 {
-                    irany = "észak";
+                    if (ablak == 1)
+                    {
+                        irany = "észak";
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nem tudsz menni semerre.");
+                        Console.ReadLine();
+                    }
                 }
                 if (lepes == "mentes")
                 {
                     string leltar = leltár1 + leltár2;
                     mentes(leltar);
+                }
+                if (lepes =="nézd")
+                {
+                    Console.WriteLine("Te egy szobában vagy ahol egy ablak látható, valószinűleg ki tudod törni a kezeddel.\nTovábblépéshez nyomj egy gombot");
+                    Console.ReadLine();
+                }
+                if (lepes == "vedd fel")
+                {
+                    bot = 1;
                 }
                 Console.Clear();
             }
