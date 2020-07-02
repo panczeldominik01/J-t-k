3eef6c5e44ef1064433eb1eb9a2673b376521472
panczeldominik01 <panczdomii@gmail.com>
2020-07-02 11:27:07 +0200

    Third Commit

--- Szabaduloszoba/Szabaduloszoba/Program.cs
+++ Szabaduloszoba/Szabaduloszoba/Program.cs
@@ -1,69 +1,201 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Diagnostics.Tracing;
 using System.IO;
+using System.Threading;
 
 namespace Szabaduloszoba
 {
     class Program
     {
-        static void mentes(string leltar)
+        
+        static string elso;
+        static string masodik;
+        static string harmadik;
+        static int kulcs = 0;
+        static int feszitovas = 0;
+        static int doboznyitas = 0;
+        static int kijutas = 0;
+        static int szekrenymozgas = 0;
+        static string helyzet = "nappali";
+        #region Simanezes
+        static void nez()
         {
-            StreamWriter write = new StreamWriter("mentes.txt");
-            write.WriteLine(leltar);          
-            write.Close();
+            if (helyzet =="nappali")
+            {
+                
+                    Console.WriteLine("A nappaliban tartózkodsz. Itt egy szekrény látható. Nyugatra a fürdőszoba");
+               
+            }
+            if (helyzet == "furdo")
+            {
+                Console.WriteLine("A fürdőbeen tartózkodsz. Itt található egy kád. Keletre a nappali.");
+            }
+
         }
+        #endregion
+        #region Nezese
+        static void dolgoknezese(int mit)
+        {
+            if (mit == 1 && helyzet=="nappali")
+            {
+                Console.WriteLine("A szekrényen nem látszik semmi, csak az, hogy kinyitható.");
+            }
+            if (mit ==2&& helyzet == "furdo")
+            {
+                Console.WriteLine("A kádban egy feszítővasat látsz.");
+            }
+            if (mit == 3&& helyzet == "nappali" && szekrenymozgas ==1)
+            {
+                Console.WriteLine("Az ablak egy jó menekülési útvonalnak tűnik. Valahogyan be kell törni.");
+            }
+        }
+        #endregion
+        #region veddfel
+        static void veddfel(int h = 0)
+        {
+            if (h==1 && doboznyitas==1)
+            {
+                Console.WriteLine("Felvetted a kulcsot. Mostnatól a leltáradban megtalálható, amíg le nem teszed.");
+                kulcs = 1;
+            }
+            if (h!=1 || doboznyitas!=1)
+            {
+                Console.WriteLine("Nem tudod felvenni a tárgyat amit beírtál.");
+            }
+
+        }
+        #endregion
+        #region nyisd
+        static void nyisd(int t = 0)
+        {
+            if (t == 1)
+            {
+                Console.WriteLine("Kinyitottad a szekrényt. Benne egy dobozt látsz.");
+            }
+            if (t == 2 && kulcs == 1)
+            {
+                Console.WriteLine("Kinyitottad az ajtót, át tudsz menni a fürdőszobába.");
+            }
+            if (t == 2 && kulcs !=1)
+            {
+                Console.WriteLine("Az ajtó nyitásához szükséged van egy kulcsra.");
+            }
+            if (t == 3)
+            {
+                Console.WriteLine("Kinyitottad a dobozt, egy kulcsot látsz benne.");
+                doboznyitas = 1;
+            }
+            else if(t==0)
+            {
+                Console.WriteLine("Ezt a tárgyat nem tudod kinyitni");
+            }
+
+
+        }
+        #endregion
         static void Main(string[] args)
         {
-            string leltár1 = "";
-            string leltár2 = "";          
-            int ablak = 0;
-            int bot = 0;
-            string irany = " ";
-            while (ablak == 0 || irany != "észak")
+            
+            while (kijutas != 1)
             {
-                Console.WriteLine("Leltár : {0}  {1}",leltár1, leltár2);
-                Console.WriteLine("Mit szerentél tenni? (nézd, menj, tör, mentes vedd fel)");
-                string lepes = Convert.ToString(Console.ReadLine());
-                if (lepes == "tör")
+                int i = 0;
+                for (i = 0; i < 3; i++)
                 {
-                    ablak = 1;
-                    if (leltár1 == "")
+                    if (elso == "*" || masodik == "*" || harmadik == "*")
                     {
-                        leltár1 = "törmelék";
+                        i = 3;
                     }
-                    else if (leltár2 =="")
+                    if (i ==0)
                     {
-                        leltár2 = "törmelék";
+                        Console.WriteLine("Adja meg mit szeretne tenni.\nAbban az esetben ha nem 3 szavas kifehezést ad meg kérem az üres helyen írja be a következőt = '*'");
+                        elso = Convert.ToString(Console.ReadLine());
+                        Console.Clear();
                     }
-                }
-                if (lepes == "menj")
-                {
-                    if (ablak == 1)
+                    if (i == 1)
                     {
-                        irany = "észak";
+                        Console.WriteLine("Adja meg mit szeretne tenni.");
+                        Console.Write("{0} ",elso);
+                        masodik = Convert.ToString(Console.ReadLine());
+                        Console.Clear();
                     }
-                    else
+                    if (i == 2)
                     {
-                        Console.WriteLine("Nem tudsz menni semerre.");
-                        Console.ReadLine();
+                        Console.WriteLine("Adja meg mit szeretne tenni.");
+                        Console.Write("{0} {1} ",elso,masodik);
+                         harmadik = Convert.ToString(Console.ReadLine());
+                        Console.Clear();
+                    }                   
+                }
+                if (elso =="nyisd")
+                {
+                    if (masodik =="szekrény")
+                    {
+                        nyisd(1);
+                        
+                    }
+                    if (masodik == "ajtó")
+                    {
+                        nyisd(2);
+                    }
+                    if (masodik == "doboz")
+                    {
+                        nyisd(3);
+                    }
+                    
+                }
+                if (elso=="vedd fel")
+                {
+                    if (masodik =="kulcs")
+                    {
+                        veddfel(1);
                     }
                 }
-                if (lepes == "mentes")
+                if (elso =="nézd" || elso =="néz")
                 {
-                    string leltar = leltár1 + leltár2;
-                    mentes(leltar);
+                    if (masodik == "szekrény")
+                    {
+                        dolgoknezese(1);
+                    }
+                    if (masodik =="kád")
+                    {
+                        dolgoknezese(2);
+                    }
+                    if (masodik =="ablak")
+                    {
+                        dolgoknezese(3);
+                    }
+                    else if(masodik =="*")
+                    {
+                        nez();
+
+                    }
                 }
-                if (lepes =="nézd")
+                if (elso=="leltár")
                 {
-                    Console.WriteLine("Te egy szobában vagy ahol egy ablak látható, valószinűleg ki tudod törni a kezeddel.\nTovábblépéshez nyomj egy gombot");
-                    Console.ReadLine();
+                    Console.Write("A leltáradban ");
+                    if (kulcs == 1)
+                    {
+                        Console.Write("kulcs");
+                    }
+                    if (kulcs == 1&& feszitovas == 1)
+                    {
+                        Console.Write(",");
+                    }
+                    if (feszitovas == 1)
+                    {
+                        Console.Write(" feszitővas");
+                    }
+                    Console.Write(" található");
                 }
-                if (lepes == "vedd fel")
-                {
-                    bot = 1;
-                }
+                elso = " "; masodik = " "; harmadik = " ";
+                i = 0;
+                Console.ReadLine();
                 Console.Clear();
             }
-            Console.WriteLine("Gratulálok kijutottál!");
+            Console.WriteLine("Gratulálok kijutottál!!");
+            Console.ReadKey();
+
         }
     }
 }
